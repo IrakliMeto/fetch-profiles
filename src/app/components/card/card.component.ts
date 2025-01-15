e3d01dd7e97bfe72abbec62b3e83dd53aec9e974
Irakli Metonidze <64604546+IrakliMeto@users.noreply.github.com>
2025-01-15 16:28:28 +0400

    add regex to age input

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,15 +1,15 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ModalConfirm } from '../modal-confirm/modal.component';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, Input, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { ModalConfirm } from "../modal-confirm/modal.component";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-card',
+  selector: "app-card",
   standalone: true,
   imports: [CommonModule, ModalConfirm, FormsModule],
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss'],
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.scss"],
 })
 export class CardComponent {
   public isEdit: boolean = false;
@@ -26,11 +26,11 @@ export class CardComponent {
   private modalService = inject(NgbModal);
 
   get removeButtonText(): string {
-    return this.isEdit ? 'Cancel' : 'Remove';
+    return this.isEdit ? "Cancel" : "Remove";
   }
 
   get editButtonText(): string {
-    return this.isEdit ? 'Save' : 'Edit';
+    return this.isEdit ? "Save" : "Edit";
   }
 
   toggleEdit() {
@@ -42,5 +42,14 @@ export class CardComponent {
       return this.toggleEdit();
     }
     this.modalService.open(ModalConfirm);
+  }
+
+  validateNumber(event: any): void {
+    const input = event.target;
+    const regex = /^\d{0,3}$/;
+
+    if (!regex.test(input.value)) {
+      input.value = input.value.slice(0, -1);
+    }
   }
 }
